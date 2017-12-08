114d165babf7b1a9d8ae1960b332b0d2edd7cd08
Caitlin Barron <barroncn@gmail.com>
2017-12-08 01:07:34 +0000

    Final Submission

--- javascript&sql/bamazonCustomer.js
+++ javascript&sql/bamazonCustomer.js
@@ -1,6 +1,9 @@
+//CHALLENGE #1: CUSTOMER 
+
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
+//use mysql to make connection variable with appropriate info
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -11,27 +14,37 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
+//Connect to the bamazon database
 connection.connect(function(err) {
   if (err) throw err;
+  
   console.log("\nWelcome to the Bamazon Store!\n");
   customerApp();
 });
 
+// customerApp is called once the connection with the database is made
 function customerApp(){
+    //Create an empty array so we can store the formatted query results (setting this array variable allows us to validate the users ID input below)
     var arr = [];
+    //Select all the information from the products table to show the customer their options
     connection.query("SELECT * FROM products", function(err,res){
         if (err) throw err;
+
         console.log("\nInventory for sale: \n==========================================");
+        //Loop through the results of the product query and push the formatted information to the arr array
         for(var i = 0; i < res.length; i++){
             arr.push("\nItem: " + res[i].product_name +  "\nID: " + res[i].item_id + "\nDepartment: " + res[i].department_name + "\nPrice: $" + res[i].price + "\n\n---------------------------\n");
         }
+        //Log the the products for customer review
         console.log(arr.join("") + "\n==========================================\n");
+        
+        //Ask the customer what product and how much of it they'd like
         inquirer.prompt([
         {
            name : "choice",
            message : "Please enter the ID number of the item you'd like to purchase: ",
            validate : function(value){
-               if (!isNaN(value) && value>0 && value<=arr.length){
+               if (!isNaN(value) && value>0 && value<=arr.length){ //Ensure the user is submitting a valid id: must be a number greater than zero and less than or equal to the arr array's length (the length of the array === the max ID number)
                    return true;
                }
                else{
@@ -44,44 +57,52 @@ function customerApp(){
             message: "How many would you like to buy?"
         }
         ]).then(function(answers){
-            connection.query("SELECT * FROM products WHERE item_id=?", [parseInt(answers.choice)], function(err, res){
+            //Get the product information for the selected item the customer wants
+            connection.query("SELECT * FROM products WHERE item_id=?", [parseInt(answers.choice)], function(err, result){
                 if (err) throw err;
-                if(answers.quantity <= res[0].stock_quantity){
+                //If the stocked quantity is sufficient to fill the customers request
+                if(answers.quantity <= result[0].stock_quantity){
+                    //Update the selected product
                     connection.query("UPDATE products SET ? WHERE ?", 
-                    [
-                        {
-                            stock_quantity: (res[0].stock_quantity - answers.quantity),
-                            number_sold: (res[0].number_sold + parseInt(answers.quantity)),
-                            revenue: ((parseInt(answers.quantity) + res[0].number_sold) * res[0].price)
+                        [{
+                            stock_quantity: (result[0].stock_quantity - answers.quantity),//use stock quantity from the selected item query result
+                            number_sold: (result[0].number_sold + parseInt(answers.quantity)),//use number sold from the selected item query result
+                            revenue: ((parseInt(answers.quantity) + result[0].number_sold) * result[0].price)//revenue will be the total items sold * the price
                         },
                         {
                            item_id: answers.choice 
-                        }
-                    ], 
+                        }], 
                     function(err, result){
                         if(err) throw err;
-                        connection.query("UPDATE departments SET ? WHERE ?", [{revenue: answers.quantity},{department_name:res.department_name}])
+                        //Log successful completion
                         console.log("\nTransaction Completed! \nPurchase Total: $" + (parseInt(answers.quantity) * res[0].price).toFixed(2) + "\nEnjoy your new " + res[0].product_name + "!\n");
+                        //Ask if the customer would like to make another purchase
                         inquirer.prompt([
-                        {
-                            type : "confirm",
-                            name : "tryAgain",
-                            message : "Would you like to make another transaction?",
-                            default: true
-                        }
-                ]).then(function(answers){
-                    if(answers.tryAgain){
-                        customerApp();
-                    }
-                    else{
-                        console.log("\nThank you for stopping by. Come back soon!\n");
-                        connection.end();
-                    }
-                });
+                            {
+                                type : "confirm",
+                                name : "another",
+                                message : "Would you like to make another transaction?",
+                                default: true
+                            }
+                        ]).then(function(answers){
+                            //if the customer wants to make another purchase
+                            if(answers.another){
+                                //run the customerApp again so they can see selection and make choice
+                                customerApp();
+                            }
+                            //if the customer is finished, say goodbye and end the connection
+                            else{
+                                console.log("\nThank you for stopping by. Come back soon!\n");
+                                connection.end();
+                            }
+                        });
                     });
                 }
+                //If the customer wants to buy more items than are available
                 else{
-                    console.log("\nInsufficient quantity. There are " + res[0].stock_quantity + " of this item remaining.\n");
+                    //Tell them how many items are remaining in case they want to purchase what is left
+                    console.log("\nInsufficient quantity. There are " + result[0].stock_quantity + " of this item remaining.\n");
+                    //Ask if they would like to make another transaction
                     inquirer.prompt([
                         {
                             type : "confirm",
@@ -89,17 +110,18 @@ function customerApp(){
                             message : "Would you like to try a different transaction?",
                             default: true
                         }
-                ]).then(function(answers){
-                    if(answers.tryAgain){
-                        customerApp();
-                    }
-                    else{
-                        console.log("\nThank you for stopping by. Come back soon!\n");
-                        connection.end();
-                    }
-                });
+                    ]).then(function(answers){
+                        //If the customer would like to try again run the customerApp to take them to the beginning
+                        if(answers.tryAgain){
+                            customerApp();
+                        }
+                        //If the customer is finished, end the connection
+                        else{
+                            console.log("\nThank you for stopping by. Come back soon!\n");
+                            connection.end();
+                        }
+                    });
                 }
-                
             });
         });
     });
--- javascript&sql/bamazonManager.js
+++ javascript&sql/bamazonManager.js
@@ -1,4 +1,5 @@
-// Challenge #2: Manager View
+//CHALLENGE #2: MANAGER
+
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
@@ -12,13 +13,17 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
+//Connect with the mysql database
 connection.connect(function(err) {
   if (err) throw err;
+  
   console.log("\nWelcome to the Bamazon Store!\n");
   managerApp();
 });
 
+//Once the connection has been made, the managerApp is called
 function managerApp(){
+    //Manager selects from a list of options
     inquirer.prompt([
         {
            type : "list",
@@ -32,41 +37,50 @@ function managerApp(){
                      ]
         }
     ]).then(function(answers){
-        if(answers.list === "view"){
-            viewInventory();
-        }
-        else if(answers.list === "low"){
-            viewLow();
-        }
-        else if(answers.list === "add"){
-            addInv();
-        }
-        else{
-            addNew();
+        //Depending on the users choice, the correct function is called
+        var userChoice = answers.list;
+        
+        switch(userChoice){
+            
+            case "view" : return viewInventory();
+            
+            case "low" : return viewLow();
+            
+            case "add" : return addInv();
+            
+            case "new" : return addNew();
         }
     });
 }
 
-//If a manager selects View Products for Sale, the app should list every available item: the item IDs, names, prices, and quantities.
+//If a manager selects View Products for Sale, the app lists every available item: the item IDs, names, prices, and quantities.
 function viewInventory(){
+    //Query the database for the item id, name, department, price and stock quantity
     connection.query("SELECT item_id, product_name, department_name, price, stock_quantity FROM products", function(err, res){
         if (err) throw err;
+        
         console.log("\nCurrent Inventory:\n==========================");
+        //Loop through the results to log formatted results to the console
         for(var i = 0; i < res.length; i++){
-            console.log("\nID: " + res[i].item_id + "\nName: " + res[i].product_name + "\nDepartment: " + res[i].department_name + "\nPrice: " + res[i].price + "\nQuantity: " + res[i].stock_quantity + "\n\n----------------------") 
+            console.log("\nID: " + res[i].item_id + "\nName: " + res[i].product_name + "\nDepartment: " + res[i].department_name + "\nPrice: " + res[i].price + "\nQuantity: " + res[i].stock_quantity + "\n\n----------------------"); 
         }
+        //Ask if they'd like to make any other inquiries
         anythingElse();
     });
 }
 
-// If a manager selects View Low Inventory, then it should list all items with an inventory count lower than five.
+// If a manager selects View Low Inventory, then it lists all items with an inventory count lower than five.
 function viewLow(){
+    //Qery the database for item information where the stock quantity is below 5
     connection.query("SELECT item_id, product_name, department_name, price, stock_quantity FROM products WHERE (stock_quantity < 5)", function(err, res){
         if (err) throw err;
+        
         console.log("\nLow Inventory:\n==========================");
+        //Loop through the results to log formatted results to the console
         for(var i = 0; i < res.length; i++){
-            console.log("\nID: " + res[i].item_id + "\nName: " + res[i].product_name + "\nDepartment: " + res[i].department_name + "\nPrice: " + res[i].price + "\nQuantity: " + res[i].stock_quantity + "\n\n----------------------") 
+            console.log("\nID: " + res[i].item_id + "\nName: " + res[i].product_name + "\nDepartment: " + res[i].department_name + "\nPrice: " + res[i].price + "\nQuantity: " + res[i].stock_quantity + "\n\n----------------------"); 
         }
+        //Go ahead and ask if they'd like to add inventory (since they're looking at low stock this is the next logical step)
         inquirer.prompt([
             {
                 type : "confirm",
@@ -75,17 +89,19 @@ function viewLow(){
                 default : false
             }
         ]).then(function(answers){
+            //If the manager would like to add inventory, run addInv()
             if(answers.confirm){
                 addInv();
             }
             else{
+            //If the user doesn't want to add inventory, make sure there isn't another inqiury they'd like to make
                 anythingElse();
             }
         });
     });
 }
 
-// If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+// If a manager selects Add to Inventory, the app displays a prompt that will let the manager "add more" of any item currently in the store.
 function addInv(){
     inquirer.prompt([
             {
@@ -97,19 +113,26 @@ function addInv(){
                 message : "How many units are you adding?"
             }
     ]).then(function(answers){
+        //Query the database for the stock quantity of the item they'd like to add
         connection.query("SELECT stock_quantity FROM products WHERE item_id=?", [answers.ID], function(err,res){
             if (err) throw err;
-            connection.query("UPDATE products SET ? WHERE ?",[{stock_quantity : (res[0].stock_quantity += parseInt(answers.quant))}, {item_id : parseInt(answers.ID)}], function(err, res){
+            
+            //Update the stock quantity with the quantity provided by the user
+            connection.query("UPDATE products SET ? WHERE ?",[{stock_quantity : (res[0].stock_quantity + parseInt(answers.quant))}, {item_id : parseInt(answers.ID)}], function(err, res){
                 if(err) throw err;
+                
+                //Log a successful update
                 console.log("You have added " + answers.quant + " units to Item #" + answers.ID + "'s inventory");
+                //Ask if there are any other transactions they'd like to make
                 anythingElse();
             });
         });
     });
 }
 
-// If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
+// If a manager selects Add New Product, the manager can add a completely new product to the store.
 function addNew(){
+    //Get the required information to create a new column in the product table (item name, department, price, stock quantity)
     inquirer.prompt([
         {
          name : "name",
@@ -129,21 +152,27 @@ function addNew(){
         }
         
     ]).then(function(answers){
+        //Query the database to insert thenew item into the products table
         connection.query("INSERT INTO products SET ?",
         {
-         product_name : answers.name,
-         department_name : answers.dept,
-         price: parseFloat(answers.price),
-         stock_quantity : parseInt(answers.stock)
+            //Set the new product information to what the user input
+            product_name : answers.name,
+            department_name : answers.dept,
+            price: parseFloat(answers.price),
+            stock_quantity : parseInt(answers.stock)
+            
         },function(err,res){
             if (err) throw err;
+            
+            //Log a successful update
             console.log("You have successfully added '" + answers.name + "' to the " + answers.dept + " department!");
+            //Ask if the user would like to make any other inquiries
             anythingElse();
         });
     });
 }
 
-//Asks if the manager would like to do anything else. If not, ends connection
+//Asks if the manager would like to do anything else.
 function anythingElse(){
     inquirer.prompt([
         {
@@ -153,9 +182,11 @@ function anythingElse(){
            default : true
         }
     ]).then(function(answers){
+        //If so, run the managerApp again
         if(answers.confirm){
             managerApp();
         }
+        // If not, end connection
         else{
             console.log("\nHave a nice day!\n");
             connection.end();
